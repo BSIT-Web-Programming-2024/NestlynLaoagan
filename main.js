cd526ee6d4e1753742644857c93ebc4171b2cffe
Nestlyn Laoagan <nstlynlgn@gmail.com>
2024-11-12 16:40:16 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 var typed = new Typed(".text", { 
-    strings: ["Web Developer", "UI/UX Designer", "YouTuber"],
+    strings: ["Web Developer", "UI/UX Designer", "Web Designer"],
     typeSpeed: 100,
     backSpeed: 50,
     backDelay: 1000,
     loop: true
 });
+
+    // Get the hamburger icon and navbar elements
+    const hamburger = document.getElementById("hamburger");
+    const navbar = document.querySelector(".navbar");
+
+    // Add click event listener to toggle 'active' class on the navbar
+    hamburger.addEventListener("click", () => {
+        navbar.classList.toggle("active");
+    });
+
+// Function to open the modal and display the clicked image
+function openModal(imageSrc) {
+    // Get the modal and image element
+    var modal = document.getElementById("imageModal");
+    var modalImage = document.getElementById("modalImage");
+
+    // Set the image source to the clicked image's source
+    modalImage.src = imageSrc;
+
+    // Display the modal
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("imageModal");
+    modal.style.display = "none";
+}
